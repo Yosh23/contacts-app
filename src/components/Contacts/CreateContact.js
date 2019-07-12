bc4b4b939e77dea9d228ef73f33a8126c87a681f
Josh Riches <joshriches@gmail.com>
2019-07-11 20:59:48 -0600

    res pending in post

--- src/components/Contacts/CreateContact.js
+++ src/components/Contacts/CreateContact.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { buildUrl } from '../../utilities'
+
+const url = buildUrl()
 
 export default class extends React.Component {
     state = {
-        name: ''
+        name : ''
+        }
+
+    handleSubmit = async (event) => {
+        event.preventDefault()
+        await fetch(url,{
+            method: "POST",
+            body: JSON.stringify(this.state)
+        })
+        .then(res => console.log(res.json()))
+
     }
 
-    handleChange = (event) =>{
+    handleChange = (event) => {
         return (
             this.setState({
             [event.target.name] : event.target.value
@@ -16,13 +29,15 @@ export default class extends React.Component {
         return (
           <>  
             <h1>Create Contact</h1>
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label>Name:</label>
                 <input type='text' 
                        name='name'
                        placeholder='Your Name Goes Here' 
                        value={this.state.name} 
                        onChange={this.handleChange} />
+                <br/>
+                <input type="submit" value="Create Contact!" />
             </form>
             {/* <p>Is This What You Typed?</p> */}
             <p>{this.state.name}</p>
